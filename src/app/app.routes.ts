01bab43934cfa9e9e6e0ee33b6c7f2402c7a4235
avila2601 <diego2601@gmail.com>
2026-01-07 13:45:02 -0500

    hdvin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CvComponent } from './cv/cv.component';
+import { CvindustrialComponent } from './cvindustrial/cvindustrial.component';
 
 export const routes: Routes = [
-  { path: '', component: CvComponent }
+  { path: '', component: CvComponent },
+  { path: 'cv-industrial', component: CvindustrialComponent }
 ];
---
+++ src/app/cvindustrial/cvindustrial.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cvindustrial',
+  standalone: true,
+  templateUrl: './cvindustrial.component.html',
+  styleUrl: './cvindustrial.component.scss'
+})
+export class CvindustrialComponent {
+
+}
