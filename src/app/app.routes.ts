dd506842b34494bf2ae788c3ad4efbb52b697f22
avila2601 <diego2601@gmail.com>
2026-05-31 19:55:53 -0500

    Add remaining industrial CV updates and PDF templates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { CvComponent } from './cv/cv.component';
 import { CvindustrialComponent } from './cvindustrial/cvindustrial.component';
 
 export const routes: Routes = [
-  { path: '', component: CvComponent },
+  { path: '', component: CvindustrialComponent },
   { path: 'cv-industrial', component: CvindustrialComponent }
 ];
